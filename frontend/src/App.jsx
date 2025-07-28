108f0721e123a8ca4c8a0e35e91601c682c3b411
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-07-28 13:07:45 +0545

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import Dashboard from "./Dashboard";
 import Login from "./pages/login";
 import Signup from "./pages/Signup";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
