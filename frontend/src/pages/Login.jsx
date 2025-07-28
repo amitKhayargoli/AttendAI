f21aace039050ef495372de7587f0f85d9b01d92
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-07-28 13:07:31 +0545

    Login and Dashboard

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,15 +3,76 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 
 export default function Login() {
-  return (
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  console.log(API_BASE_URL);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError('');
+    setSuccess('');
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${API_BASE_URL}/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: e.target.email.value,
+          password: e.target.password.value,
+        }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || 'Login failed');
+      }
+
+      const data = await response.json();
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setSuccess('Login successful');
+        localStorage.setItem('token', data.token);
+
+        console.log(data);
+
+        if (data.user.userType === 'admin') {
+          navigate('/admin');
+        } else if (data.user.userType === 'student') {
+          navigate('/student');
+        }
+        else if (data.user.userType === 'faculty') {
+          navigate('/faculty');
+        } else {
+          navigate('/');
+        }
+
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (  
     <div className="flex w-full h-screen justify-center items-center">
       <div className="flex flex-col gap-6">
         <Card className="overflow-hidden !py-0 !border-none">
           <CardContent className="grid !p-0 md:grid-cols-2">
-            <form className="p-20 md:p-30 w-120 sm:w-140">
+            <form className="p-20 md:p-30 w-120 sm:w-140" onSubmit={handleLogin}>
               <div className="flex flex-col gap-6">
                 <div className="flex flex-col items-center text-center">
                   <h1 className="text-2xl font-bold">AttendAI</h1>
@@ -21,22 +82,21 @@ export default function Login() {
                 </div>
                 <div className="grid gap-3">
                   <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
+                  <Input id="email" name="email" type="email" required />
                 </div>
                 <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                  </div>
-                  <Input id="password" type="password" required />
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" name="password" type="password" required />
                 </div>
-                <Button type="submit" className="w-full !bg-[#9886fe]">
-                  Login
+                <Button type="submit" className="w-full !bg-[#9886fe]" disabled={isLoading}>
+                  {isLoading ? 'Logging in...' : 'Login'}
                 </Button>
+                {error && (
+                  <div className="text-red-500 text-center text-sm">{error}</div>
+                )}
+                {success && (
+                  <div className="text-green-600 text-center text-sm">{success}</div>
+                )}
 
                 <a
                   href="#"
