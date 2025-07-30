6751e62db0b98227f544c2302fb2360cabc76e5a
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-07-30 23:33:34 +0545

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,21 @@ import Dashboard from "./Dashboard";
 import Login from "./pages/login";
 import Signup from "./pages/Signup";
 import AdminDashboard from "./pages/AdminDashboard";
+import Students from "./pages/Students";
+import { useState } from "react";
 
 function App() {
+  const [userRole, setUserRole] = useState("Student");
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/students" element={<Students />} />
       </Routes>
     </Router>
   );
