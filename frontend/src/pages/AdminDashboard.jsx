5483bb265cc82ee99f12ae3a061121e113d388fa
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-07-30 23:34:35 +0545

    minor changes

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../Sidebar";
+import AdminSidebar from "../AdminSidebar";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { ChartLineDots } from "@/components/ChartLineDots";
 import EventCalendarCard from "@/components/EventCalendarCard";
@@ -54,7 +54,7 @@ function StatsCard({ label, value, svg }) {
 const AdminDashboard = () => {
   return (
     <div className="min-h-screen flex" style={{ background: "#f8f9fa" }}>
-      <Sidebar />
+      <AdminSidebar />
       <div className="flex flex-col gap-2 flex-2 border-l-2 border-[#E5E7EB]">
         <nav className="flex py-3 w-full justify-between items-center px-5 bg-white">
           <div>
